30e1978641f493b64b9487f77b026952b78f0017
David Crooks <davidacrooks@outlook.com>
2018-05-02 17:59:33 -0400

    tuple_named_vector initial

--- include/EASTL/tuple_named.h
+++ include/EASTL/tuple_named.h
@@ -7,11 +7,15 @@
 
 #include <EASTL/internal/config.h>
 #include <EASTL/tuple.h>
+#include <EASTL/tuple_vector.h>
 
 #if EASTL_TUPLE_ENABLED
 
 namespace eastl
 {
+	//////////////////////////////////////////////////////////////////////////
+	// helpers for named_tags
+
 	constexpr unsigned long hashCalc(const char* ch, size_t len)
 	{
 		return len > 0 ? ((unsigned long)(*ch) + hashCalc(ch + 1, len - 1) * 33) % (1 << 26) : 0;
@@ -21,10 +25,6 @@ namespace eastl
 		return hashCalc(ch, len);
 	}
 
-	template<typename T, size_t N>
-	class tuple_named_tag { };
-
-	// attempt to isolate index given a type
 	template <size_t val, size_t... Ns>
 	struct index_lookup_helper;
 
@@ -52,6 +52,24 @@ namespace eastl
 		static const size_t index = index_lookup_helper<val, Ns...>::index + 1;
 	};
 
+	// dcrooks-todo trying to make tuple_named_tag convertible between, e.g., T and T&
+	template<typename T, size_t N>
+	class tuple_named_tag { 
+	public:
+
+		tuple_named_tag(const T& t)
+			: m_t(t)
+		{}
+
+		template <typename OtherT, size_t OtherN>
+			tuple_named_tag(tuple_named_tag<OtherT, OtherN>&& t)
+			: m_t(forward<OtherT>(t.m_t))
+		{}
+
+
+		T m_t;
+	};
+
 	template <size_t val, typename... Tags>
 	struct index_lookup;
 
@@ -61,6 +79,9 @@ namespace eastl
 		static const int index = val > sizeof...(Ts) ? index_lookup_helper<val, Ns...>::index : val;
 	};
 
+	//////////////////////////////////////////////////////////////////////////
+	// tuple_named
+
 	template<typename... Tags>
 	class tuple_named;
 
@@ -68,17 +89,163 @@ namespace eastl
 	class tuple_named<tuple_named_tag<Ts, Ns>...> : public tuple<Ts...>
 	{
 	public:
+		typedef tuple<Ts...> TupleType;
+	
 		tuple_named<tuple_named_tag<Ts, Ns>...>(const Ts&... t)
 			:tuple<Ts...>(t...)
 		{}
 
-		typedef tuple<Ts...> TupleType;
 	};
-	
+
 	template <size_t I, typename... Tags>
 	auto get(tuple_named<Tags...>& t)
 	{
 		return get<index_lookup<I, Tags...>::index>(tuple_named<Tags...>::TupleType(t));
+	}
+	
+	//////////////////////////////////////////////////////////////////////////
+	// tuple_named_vector and TupleNamedVecIter
+
+	template<typename... Tags>
+	class tuple_named_vector;
+
+	template<typename... Tags>
+	struct TupleNamedVecIter;
+
+	template <typename... Ts, size_t... Ns>
+	struct TupleNamedVecIter<tuple_named_tag<Ts, Ns>...> : public iterator<random_access_iterator_tag,
+		tuple_named<tuple_named_tag<Ts, Ns>...>,
+		ptrdiff_t,
+		tuple_named<tuple_named_tag<Ts*, Ns>...>,
+		tuple_named<tuple_named_tag<Ts&, Ns>...>
+	>
+	{
+	private:
+		typedef TupleNamedVecIter<tuple_named_tag<Ts, Ns>...> this_type;
+
+	public:
+		TupleNamedVecIter() = default;
+		TupleNamedVecIter(tuple_named_vector<tuple_named_tag<Ts, Ns>...>& tupleVec, size_t index)
+			: mTupleVec(&tupleVec), mIndex(index) { }
+
+		bool operator==(const TupleNamedVecIter& other) const { return mIndex == other.mIndex && mTupleVec->get<0>() == other.mTupleVec->get<0>(); }
+		bool operator!=(const TupleNamedVecIter& other) const { return mIndex != other.mIndex || mTupleVec->get<0>() != other.mTupleVec->get<0>(); }
+		reference operator*() { return MakeReference(make_index_sequence<sizeof...(Ts)>()); }
+
+		this_type& operator++() { ++mIndex; return *this; }
+		this_type operator++(int)
+		{
+			this_type temp = *this;
+			++mIndex;
+			return temp;
+		}
+
+		this_type& operator--() { --mIndex; return *this; }
+		this_type operator--(int)
+		{
+			this_type temp = *this;
+			--mIndex;
+			return temp;
+		}
+
+		this_type& operator+=(difference_type n) { mIndex += n; return *this; }
+		this_type operator+(difference_type n)
+		{
+			this_type temp = *this;
+			return temp += n;
+		}
+		friend this_type operator+(difference_type n, const this_type& rhs)
+		{
+			this_type temp = rhs;
+			return temp += n;
+		}
+
+		this_type& operator-=(difference_type n) { mIndex -= n; return *this; }
+		this_type operator-(difference_type n)
+		{
+			this_type temp = *this;
+			return temp -= n;
+		}
+		friend this_type operator-(difference_type n, const this_type& rhs)
+		{
+			this_type temp = rhs;
+			return temp -= n;
+		}
+
+		difference_type operator-(const this_type& rhs) { return mIndex - rhs.mIndex; }
+		bool operator<(const this_type& rhs) { return mIndex < rhs.mIndex; }
+		bool operator>(const this_type& rhs) { return mIndex > rhs.mIndex; }
+		bool operator>=(const this_type& rhs) { return mIndex >= rhs.mIndex; }
+		bool operator<=(const this_type& rhs) { return mIndex <= rhs.mIndex; }
+
+		reference operator[](size_t n)
+		{
+			return *(*this + n);
+		}
+
+	private:
+
+		template <size_t... Indices>
+		value_type MakeValue(integer_sequence<size_t, Indices...> indices)
+		{
+			return value_type(mTupleVec->get<Indices>()[mIndex]...);
+		}
+
+		template <size_t... Indices>
+		reference MakeReference(integer_sequence<size_t, Indices...> indices)
+		{
+			return reference(mTupleVec->get<Indices>()[mIndex]...);
+		}
+
+		template <size_t... Indices>
+		pointer MakePointer(integer_sequence<size_t, Indices...> indices)
+		{
+			return pointer(&mTupleVec->get<Indices>()[mIndex]...);
+		}
+
+		size_t mIndex = 0;
+		tuple_named_vector<tuple_named_tag<Ts, Ns>...> *mTupleVec = nullptr;
+	};
+
+	template<typename... Ts, size_t... Ns>
+	class tuple_named_vector<tuple_named_tag<Ts, Ns>...> : public tuple_vector<Ts...>
+	{
+	public:
+		tuple_named_vector<tuple_named_tag<Ts, Ns>...>()
+			: tuple_vector<Ts...>()
+		{}
+
+		tuple_named_vector<tuple_named_tag<Ts, Ns>...>(const Ts&... t)
+			: tuple_vector<Ts...>(t...)
+		{}
+
+		typedef tuple_vector<Ts...> TupleVectorType;
+
+		template<size_t I>
+		auto get();
+
+		template<typename T>
+		T* get()
+		{
+			return TupleVectorType::get<T>();
+		}
+
+		typedef TupleNamedVecIter<tuple_named_tag<Ts, Ns>...> iterator;
+		iterator begin()
+		{
+			return iterator(*this, 0);
+		}
+		iterator end()
+		{
+			return iterator(*this, size());
+		}
+	};
+
+	template<typename... Ts, size_t... Ns>
+	template<size_t I>
+	auto tuple_named_vector<tuple_named_tag<Ts, Ns>...>::get()
+	{
+		return TupleVectorType::get<index_lookup<I, tuple_named_tag<Ts, Ns>...>::index>();
 	}
 
 }  // namespace eastl
--- test/source/TestTupleNamed.cpp
+++ test/source/TestTupleNamed.cpp
@@ -5,6 +5,9 @@
 #include "EASTLTest.h"
 
 #include <EASTL/tuple_named.h>
+#include <random>
+#include <EASTL/sort.h>
+#include <EASTL/array.h>
 
 #if EASTL_TUPLE_ENABLED
 
@@ -46,6 +49,236 @@ int TestTupleNamed()
 		EATEST_VERIFY(get<"anotherInt"_tn>(multipleIntsNamed) == 3);
 
 
+		tuple_vector<int> singleElementVec;
+		EATEST_VERIFY(singleElementVec.size() == 0);
+		EATEST_VERIFY(singleElementVec.capacity() == 0);
+		singleElementVec.push_back_uninitialized();
+		singleElementVec.push_back(5);
+		EATEST_VERIFY(singleElementVec.size() == 2);
+		EATEST_VERIFY(singleElementVec.get<0>()[1] == 5);
+		EATEST_VERIFY(singleElementVec.get<int>()[1] == 5);
+
+		tuple_named_vector<
+			tuple_named_tag<int, "int"_tn>
+		> intNamedVec;
+		EATEST_VERIFY(intNamedVec.size() == 0);
+		EATEST_VERIFY(intNamedVec.capacity() == 0);
+		intNamedVec.push_back_uninitialized();
+		intNamedVec.push_back(5);
+		EATEST_VERIFY(intNamedVec.size() == 2);
+		EATEST_VERIFY(intNamedVec.get<0>()[1] == 5);
+		EATEST_VERIFY(intNamedVec.get<int>()[1] == 5);
+		EATEST_VERIFY(intNamedVec.get<"int"_tn>()[1] == 5);
+
+	}
+
+	// Test tuple_Vector in a ranged for, and other large-scale iterator testing
+	{
+		tuple_named_vector<
+			tuple_named_tag<int, "int"_tn>,
+			tuple_named_tag<float, "fl"_tn>,
+			tuple_named_tag<int, "int2"_tn>
+			> tripleElementVec;
+		tripleElementVec.push_back(1, 2.0f, 6);
+		tripleElementVec.push_back(2, 3.0f, 7);
+		tripleElementVec.push_back(3, 4.0f, 8);
+		tripleElementVec.push_back(4, 5.0f, 9);
+		tripleElementVec.push_back(5, 6.0f, 10);
+
+
+		// test copyConstructible, copyAssignable, swappable, prefix inc, !=, reference convertible to value_type (InputIterator!)
+		{
+			tuple_named_vector<
+				tuple_named_tag<int, "int"_tn>,
+				tuple_named_tag<float, "fl"_tn>,
+				tuple_named_tag<int, "int2"_tn>
+			>::iterator iter = tripleElementVec.begin();
+			++iter;
+			auto copiedIter(iter);
+			EATEST_VERIFY(get<2>(*copiedIter) == 7);
+			EATEST_VERIFY(copiedIter == iter);
+
+			++iter;
+			copiedIter = iter;
+			EATEST_VERIFY(get<2>(*copiedIter) == 8);
+
+			++iter;
+			swap(iter, copiedIter);
+			EATEST_VERIFY(get<2>(*iter) == 8);
+			EATEST_VERIFY(get<2>(*copiedIter) == 9);
+
+			EATEST_VERIFY(copiedIter != iter);
+
+			tuple_named_tag<int, "int"_tn> intTag(5);
+			tuple_named_tag<int&, "int"_tn> intRefTag(intTag);
+			tuple_named_tag<int, "int"_tn> intOthTag(intRefTag);
+
+
+			//tuple<int&, float&, int&> ref(*iter);
+			//tuple_named<
+			//	tuple_named_tag<int, "int"_tn>,
+			//	tuple_named_tag<float, "fl"_tn>,
+			//	tuple_named_tag<int, "int2"_tn>
+			//	> value(*iter);
+			//EATEST_VERIFY(get<2>(ref) == get<2>(value));
+		}
+
+		// test postfix increment, default constructible (ForwardIterator)
+		{
+			tuple_named_vector<
+				tuple_named_tag<int, "int"_tn>,
+				tuple_named_tag<float, "fl"_tn>,
+				tuple_named_tag<int, "int2"_tn>
+			>::iterator iter = tripleElementVec.begin();
+			auto prefixIter = ++iter;
+
+			tuple_named_vector<
+				tuple_named_tag<int, "int"_tn>,
+				tuple_named_tag<float, "fl"_tn>,
+				tuple_named_tag<int, "int2"_tn>
+			>::iterator postfixIter;
+			postfixIter = iter++;
+			EATEST_VERIFY(prefixIter == postfixIter);
+			EATEST_VERIFY(get<2>(*prefixIter) == 7);
+			EATEST_VERIFY(get<2>(*iter) == 8);
+		}
+
+		// test prefix decrement and postfix decrement (BidirectionalIterator)
+		{
+			tuple_named_vector<
+				tuple_named_tag<int, "int"_tn>,
+				tuple_named_tag<float, "fl"_tn>,
+				tuple_named_tag<int, "int2"_tn>
+			>::iterator iter = tripleElementVec.end();
+			auto prefixIter = --iter;
+
+			tuple_named_vector<
+				tuple_named_tag<int, "int"_tn>,
+				tuple_named_tag<float, "fl"_tn>,
+				tuple_named_tag<int, "int2"_tn>
+			>::iterator postfixIter;
+			postfixIter = iter--;
+			EATEST_VERIFY(prefixIter == postfixIter);
+			EATEST_VERIFY(get<2>(*prefixIter) == 10);
+			EATEST_VERIFY(get<2>(*iter) == 9);
+		}
+
+		// test many arithmetic operations (RandomAccessIterator)
+		{
+			tuple_named_vector<
+				tuple_named_tag<int, "int"_tn>,
+				tuple_named_tag<float, "fl"_tn>,
+				tuple_named_tag<int, "int2"_tn>
+			>::iterator iter = tripleElementVec.begin();
+			auto symmetryOne = iter + 2;
+			auto symmetryTwo = 2 + iter;
+			iter += 2;
+			EATEST_VERIFY(symmetryOne == symmetryTwo);
+			EATEST_VERIFY(symmetryOne == iter);
+
+			symmetryOne = iter - 2;
+			symmetryTwo = 2 - iter;
+			iter -= 2;
+			EATEST_VERIFY(symmetryOne == symmetryTwo);
+			EATEST_VERIFY(symmetryOne == iter);
+
+			iter += 2;
+			EATEST_VERIFY(iter - symmetryOne == 2);
+
+			tuple<int&, float&, int&> symmetryRef = symmetryOne[2];
+			EATEST_VERIFY(get<2>(symmetryRef) == get<2>(*iter));
+
+			EATEST_VERIFY(symmetryOne < iter);
+			EATEST_VERIFY(iter > symmetryOne);
+			EATEST_VERIFY(symmetryOne >= symmetryTwo && iter >= symmetryOne);
+			EATEST_VERIFY(symmetryOne <= symmetryTwo && symmetryOne <= iter);
+		}
+
+		{
+			float i = 0;
+			int j = 0;
+			EATEST_VERIFY(&get<0>(*tripleElementVec.begin()) == tripleElementVec.get<0>());
+			EATEST_VERIFY(&get<1>(*tripleElementVec.begin()) == tripleElementVec.get<1>());
+			for (auto& iter : tripleElementVec)
+			{
+				i += get<1>(iter);
+				j += get<2>(iter);
+			}
+			EATEST_VERIFY(i == 20.0f);
+			EATEST_VERIFY(j == 40);
+		}
+	}
+
+	// test sort.h
+	{
+		// create+populate the two vectors with some junk
+		const int ElementCount = 1 * 1024 * 1024;
+		const int NumData = 64;
+		struct LargeData
+		{
+			LargeData(float f)
+			{
+				data.fill(f);
+			}
+			eastl::array<float, NumData> data;
+		};
+		tuple_named_vector<
+			tuple_named_tag<bool, "isActive"_tn>,
+			tuple_named_tag<LargeData, "payload"_tn>, 
+			tuple_named_tag<int, "lifetime"_tn>
+		> tripleElementVec;
+		tripleElementVec.reserve(ElementCount);
+
+		struct TripleElement
+		{
+			bool a;
+			LargeData b;
+			int c;
+		};
+		vector<TripleElement> aosTripleElement;
+		aosTripleElement.reserve(ElementCount);
+
+		std::default_random_engine e1(0);
+		std::uniform_int_distribution<int> bool_picker(0, 1);
+		std::uniform_real_distribution<float> float_picker(0, 32768);
+		std::uniform_int_distribution<int> int_picker(0, 32768);
+
+		for (int i = 0; i < ElementCount; ++i)
+		{
+			bool randomBool = bool_picker(e1) < 1 ? false : true;
+			float randomFloat = float_picker(e1);
+			int randomInt = int_picker(e1);
+			tripleElementVec.push_back(randomBool, { randomFloat }, randomInt);
+			aosTripleElement.push_back({ randomBool,{ randomFloat }, randomInt });
+		}
+
+
+		// measure tuplevec in a loop
+		volatile int numTupleBools = 0;
+		for (auto& iter : tripleElementVec)
+		{
+			numTupleBools += get<"isActive"_tn>(iter) ? 1 : 0;
+		}
+
+		//// measure tuplevec in a sort
+		//sort(tripleElementVec.begin(), tripleElementVec.end(),
+		//	[](auto& a, auto& b)
+		//{
+		//	return get<>(a) > get<2>(b);
+		//});
+
+		//// measure vector in a loop
+		//volatile int numVecBools = 0;
+		//for (auto& iter : aosTripleElement)
+		//{
+		//	numVecBools += iter.a ? 1 : 0;
+		//}
+
+		//// measure vector in a sort
+		//sort(aosTripleElement.begin(), aosTripleElement.end(), [](const TripleElement& a, const TripleElement& b)
+		//{
+		//	return a.c > b.c;
+		//});
 
 	}
 
